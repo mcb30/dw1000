8a3a91d60d125050d41a5b93fa7ede6d9ad461d7
Michael Brown <mbrown@fensystems.co.uk>
2018-02-08 16:18:18 +0000

    kernel: Expose all registers via regmap
    Allow all registers to be inspected via debugfs, e.g.:
    
      cat /sys/kernel/debug/regmap/spi0.0-pmsc/registers
    
    Give up on using regmap as anything other than a debugging
    abstraction, since (a) the register map of the DW1000 is absurdly
    irregular, and (b) the async regmap API cannot be used by
    out-of-kernel drivers.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- kernel/dw1000.c
+++ kernel/dw1000.c
@@ -27,6 +27,279 @@
 #include <net/mac802154.h>
 #include "dw1000.h"
 
+/********************************************************************************
+ *
+ * Register access
+ *
+ */
+
+/* SPI message */
+struct dw1000_message {
+	struct spi_message msg;
+	struct spi_transfer xfer[2];
+	struct dw1000_spi_header header;
+};
+
+/* Construct register address */
+static struct spi_transfer * dw1000_message_init(struct dw1000_message *msg,
+						 unsigned int file,
+						 unsigned int offset)
+{
+	struct spi_transfer *xfer;
+
+	/* Construct header */
+	spi_message_init(&msg->msg);
+	xfer = &msg->xfer[0];
+	xfer->tx_buf = &msg->header;
+	msg->header.command = file;
+	if (likely(offset == 0)) {
+		xfer->len = offsetofend(typeof(msg->header), command);
+	} else if (offset < DW1000_EXTENDED) {
+		msg->header.command |= DW1000_OFFSET;
+		msg->header.low = offset;
+		xfer->len = offsetofend(typeof(msg->header), low);
+	} else {
+		msg->header.command |= DW1000_OFFSET;
+		msg->header.low = (offset | DW1000_EXTENDED);
+		msg->header.high = (offset >> DW1000_EXTENDED_SHIFT);
+		xfer->len = offsetofend(typeof(msg->header), high);
+	}
+	spi_message_add_tail(xfer++, &msg->msg);
+
+	return xfer;
+}
+
+/* Read register(s) */
+static int dw1000_read(struct dw1000 *dw, unsigned int file, unsigned int offset,
+		       void *data, size_t len)
+{
+	struct dw1000_message msg;
+	struct spi_transfer *xfer;
+
+	/* Construct message */
+	memset(&msg, 0, sizeof(msg));
+	xfer = dw1000_message_init(&msg, file, offset);
+	xfer->rx_buf = data;
+	xfer->len = len;
+	spi_message_add_tail(xfer, &msg.msg);
+
+	return spi_sync(dw->spi, &msg.msg);
+}
+
+/* Write register(s) */
+static int dw1000_write(struct dw1000 *dw, unsigned int file, unsigned int offset,
+			const void *data, size_t len)
+{
+	struct dw1000_message msg;
+	struct spi_transfer *xfer;
+
+	/* Construct message */
+	memset(&msg, 0, sizeof(msg));
+	xfer = dw1000_message_init(&msg, file, offset);
+	msg.header.command |= DW1000_WRITE;
+	xfer->tx_buf = data;
+	xfer->len = len;
+	spi_message_add_tail(xfer, &msg.msg);
+
+	return spi_sync(dw->spi, &msg.msg);
+}
+
+/* Write register(s) asynchronously */
+static int dw1000_write_async(struct dw1000 *dw, unsigned int file,
+			      unsigned int offset, const void *data, size_t len,
+			      struct dw1000_message *msg,
+			      void (*complete)(struct dw1000 *dw))
+{
+	struct spi_transfer *xfer;
+
+	/* Construct message */
+	memset(msg, 0, sizeof(*msg));
+	xfer = dw1000_message_init(msg, file, offset);
+	msg->header.command |= DW1000_WRITE;
+	xfer->tx_buf = data;
+	xfer->len = len;
+	spi_message_add_tail(xfer, &msg->msg);
+
+	/* Prepare completion */
+	msg->msg.complete = (void (*)(void *))complete;
+	msg->msg.context = dw;
+
+	return spi_async(dw->spi, &msg->msg);
+}
+
+/********************************************************************************
+ *
+ * Register map abstraction
+ *
+ */
+
+/* Write register(s) */
+static int dw1000_regmap_write(void *context, const void *data, size_t len)
+{
+	struct dw1000_regmap *map = context;
+	const uint16_t *offset;
+
+	/* Extract offset */
+	if (len < sizeof(*offset))
+		return -EINVAL;
+	offset = data;
+	data += sizeof(*offset);
+	len -= sizeof(*offset);
+
+	return dw1000_write(map->dw, map->config->file, *offset, data, len);
+}
+
+/* Read register(s) */
+static int dw1000_regmap_read(void *context, const void *reg, size_t reg_len,
+			      void *val, size_t val_len)
+{
+	struct dw1000_regmap *map = context;
+	const uint16_t *offset;
+
+	/* Extract offset */
+	if (reg_len != sizeof(*offset))
+		return -EINVAL;
+	offset = reg;
+
+	return dw1000_read(map->dw, map->config->file, *offset, val, val_len);
+}
+
+/* Register map bus */
+static const struct regmap_bus dw1000_regmap_bus = {
+	.write = dw1000_regmap_write,
+	.read = dw1000_regmap_read,
+	.reg_format_endian_default = REGMAP_ENDIAN_NATIVE,
+	.val_format_endian_default = REGMAP_ENDIAN_NATIVE,
+};
+
+/* Register map configurations */
+#define DW1000_REGMAP(_name, _file, _len, _type, ...)			\
+	static const struct dw1000_regmap_config dw1000_ ## _name = {	\
+		.offset = offsetof(struct dw1000, _name),		\
+		.file =	_file,						\
+		.shift = ilog2(sizeof(_type)),				\
+		.config = {						\
+			.name = #_name,					\
+			.reg_bits = DW1000_REG_BITS,			\
+			.reg_stride = sizeof(_type),			\
+			.val_bits = (8 * sizeof(_type)),		\
+			.max_register = ((_len) - sizeof(_type)),	\
+			__VA_ARGS__					\
+		},							\
+	}
+DW1000_REGMAP(dev_id, DW1000_DEV_ID, DW1000_DEV_ID_LEN, uint32_t );
+DW1000_REGMAP(eui, DW1000_EUI, DW1000_EUI_LEN, uint8_t );
+DW1000_REGMAP(panadr, DW1000_PANADR, DW1000_PANADR_LEN, uint16_t );
+DW1000_REGMAP(sys_cfg, DW1000_SYS_CFG, DW1000_SYS_CFG_LEN, uint32_t );
+DW1000_REGMAP(sys_time, DW1000_SYS_TIME, DW1000_SYS_TIME_LEN, uint8_t );
+DW1000_REGMAP(tx_fctrl, DW1000_TX_FCTRL, DW1000_TX_FCTRL_LEN, uint8_t );
+DW1000_REGMAP(tx_buffer, DW1000_TX_BUFFER, DW1000_TX_BUFFER_LEN, uint8_t );
+DW1000_REGMAP(dx_time, DW1000_DX_TIME, DW1000_DX_TIME_LEN, uint8_t );
+DW1000_REGMAP(rx_fwto, DW1000_RX_FWTO, DW1000_RX_FWTO_LEN, uint16_t );
+DW1000_REGMAP(sys_ctrl, DW1000_SYS_CTRL, DW1000_SYS_CTRL_LEN, uint32_t );
+DW1000_REGMAP(sys_mask, DW1000_SYS_MASK, DW1000_SYS_MASK_LEN, uint8_t );
+DW1000_REGMAP(sys_status, DW1000_SYS_STATUS, DW1000_SYS_STATUS_LEN, uint8_t );
+DW1000_REGMAP(rx_finfo, DW1000_RX_FINFO, DW1000_RX_FINFO_LEN, uint32_t );
+DW1000_REGMAP(rx_buffer, DW1000_RX_BUFFER, DW1000_RX_BUFFER_LEN, uint8_t );
+DW1000_REGMAP(rx_fqual, DW1000_RX_FQUAL, DW1000_RX_FQUAL_LEN, uint16_t );
+DW1000_REGMAP(rx_ttcki, DW1000_RX_TTCKI, DW1000_RX_TTCKI_LEN, uint32_t );
+DW1000_REGMAP(rx_ttcko, DW1000_RX_TTCKO, DW1000_RX_TTCKO_LEN, uint8_t );
+DW1000_REGMAP(rx_time, DW1000_RX_TIME, DW1000_RX_TIME_LEN, uint8_t );
+DW1000_REGMAP(tx_time, DW1000_TX_TIME, DW1000_TX_TIME_LEN, uint8_t );
+DW1000_REGMAP(tx_antd, DW1000_TX_ANTD, DW1000_TX_ANTD_LEN, uint16_t );
+DW1000_REGMAP(ack_resp_t, DW1000_ACK_RESP_T, DW1000_ACK_RESP_T_LEN, uint32_t );
+DW1000_REGMAP(rx_sniff, DW1000_RX_SNIFF, DW1000_RX_SNIFF_LEN, uint32_t );
+DW1000_REGMAP(tx_power, DW1000_TX_POWER, DW1000_TX_POWER_LEN, uint8_t );
+DW1000_REGMAP(chan_ctrl, DW1000_CHAN_CTRL, DW1000_CHAN_CTRL_LEN, uint32_t );
+DW1000_REGMAP(usr_sfd, DW1000_USR_SFD, DW1000_USR_SFD_LEN, uint8_t );
+DW1000_REGMAP(agc_ctrl, DW1000_AGC_CTRL, DW1000_AGC_CTRL_LEN, uint8_t );
+DW1000_REGMAP(ext_sync, DW1000_EXT_SYNC, DW1000_EXT_SYNC_LEN, uint32_t );
+DW1000_REGMAP(acc_mem, DW1000_ACC_MEM, DW1000_ACC_MEM_LEN, uint32_t );
+DW1000_REGMAP(gpio_ctrl, DW1000_GPIO_CTRL, DW1000_GPIO_CTRL_LEN, uint32_t );
+DW1000_REGMAP(drx_conf, DW1000_DRX_CONF, DW1000_DRX_CONF_LEN, uint16_t );
+DW1000_REGMAP(rf_conf, DW1000_RF_CONF, DW1000_RF_CONF_LEN, uint32_t );
+DW1000_REGMAP(tx_cal, DW1000_TX_CAL, DW1000_TX_CAL_LEN, uint8_t );
+DW1000_REGMAP(fs_ctrl, DW1000_FS_CTRL, DW1000_FS_CTRL_LEN, uint8_t );
+DW1000_REGMAP(aon, DW1000_AON, DW1000_AON_LEN, uint8_t );
+DW1000_REGMAP(otp_if, DW1000_OTP_IF, DW1000_OTP_IF_LEN, uint16_t );
+DW1000_REGMAP(lde_ctrl, DW1000_LDE_CTRL, DW1000_LDE_CTRL_LEN, uint16_t );
+DW1000_REGMAP(dig_diag, DW1000_DIG_DIAG, DW1000_DIG_DIAG_LEN, uint16_t );
+DW1000_REGMAP(pmsc, DW1000_PMSC, DW1000_PMSC_LEN, uint16_t );
+
+static const struct dw1000_regmap_config *dw1000_configs[] = {
+	&dw1000_dev_id,
+	&dw1000_eui,
+	&dw1000_panadr,
+	&dw1000_sys_cfg,
+	&dw1000_sys_time,
+	&dw1000_tx_fctrl,
+	&dw1000_tx_buffer,
+	&dw1000_dx_time,
+	&dw1000_rx_fwto,
+	&dw1000_sys_ctrl,
+	&dw1000_sys_mask,
+	&dw1000_sys_status,
+	&dw1000_rx_finfo,
+	&dw1000_rx_buffer,
+	&dw1000_rx_fqual,
+	&dw1000_rx_ttcki,
+	&dw1000_rx_ttcko,
+	&dw1000_rx_time,
+	&dw1000_tx_time,
+	&dw1000_tx_antd,
+	&dw1000_ack_resp_t,
+	&dw1000_rx_sniff,
+	&dw1000_tx_power,
+	&dw1000_chan_ctrl,
+	&dw1000_usr_sfd,
+	&dw1000_agc_ctrl,
+	&dw1000_ext_sync,
+	&dw1000_acc_mem,
+	&dw1000_gpio_ctrl,
+	&dw1000_drx_conf,
+	&dw1000_rf_conf,
+	&dw1000_tx_cal,
+	&dw1000_fs_ctrl,
+	&dw1000_aon,
+	&dw1000_otp_if,
+	&dw1000_lde_ctrl,
+	&dw1000_dig_diag,
+	&dw1000_pmsc,
+};
+
+/* Initialise register maps */
+static int dw1000_regmap_init(struct dw1000 *dw)
+{
+	const struct dw1000_regmap_config *config;
+	struct dw1000_regmap *map;
+	unsigned int i;
+	int rc;
+
+	/* Initialise register maps */
+	for (i = 0; i < ARRAY_SIZE(dw1000_configs); i++) {
+		config = dw1000_configs[i];
+		map = (void *)dw + config->offset;
+		map->dw = dw;
+		map->config = config;
+		map->regs = devm_regmap_init(&dw->spi->dev, &dw1000_regmap_bus,
+					     map, &config->config);
+		if (IS_ERR(map->regs)) {
+			rc = PTR_ERR(map->regs);
+			dev_err(&dw->spi->dev, "could not allocate %s map: %d\n",
+				config->config.name, rc);
+			return rc;
+		}
+	}
+
+	return 0;
+}
+
+/********************************************************************************
+ *
+ * IEEE 802.15.4 interface
+ *
+ */
+
 static int dw1000_start(struct ieee802154_hw *hw)
 {
 	struct dw1000 *dw = hw->priv;
@@ -75,37 +348,6 @@ static const struct ieee802154_ops dw1000_ops = {
 
 /********************************************************************************
  *
- * Register map
- *
- */
-
-static const struct regmap_config dw1000_regmap_dword = {
-	.name = "dw1000-dword",
-	.reg_bits = DW1000_DWORD_REG_BITS,
-	.pad_bits = DW1000_DWORD_PAD_BITS,
-	.val_bits = DW1000_DWORD_VAL_BITS,
-	.val_format_endian = REGMAP_ENDIAN_LITTLE,
-	.write_flag_mask = DW1000_WRITE_FLAG_MASK,
-	.read_flag_mask = DW1000_READ_FLAG_MASK,
-};
-
-static int dw1000_regmap_init(struct dw1000 *dw)
-{
-	int rc;
-
-	/* Initialise dword register map */
-	dw->regs.dword = devm_regmap_init_spi(dw->spi, &dw1000_regmap_dword);
-	if (IS_ERR(dw->regs.dword)) {
-		rc = PTR_ERR(dw->regs.dword);
-		dev_err(&dw->spi->dev, "could not allocate dword map: %d\n", rc);
-		return rc;
-	}
-
-	return 0;
-}
-
-/********************************************************************************
- *
  * Device initialisation
  *
  */
@@ -114,7 +356,7 @@ static int dw1000_probe(struct spi_device *spi)
 {
 	struct ieee802154_hw *hw;
 	struct dw1000 *dw;
-	unsigned int id;
+	struct dw1000_dev_id id;
 	int rc;
 
 	/* Allocate IEEE 802.15.4 device */
@@ -127,18 +369,25 @@ static int dw1000_probe(struct spi_device *spi)
 	dw->spi = spi;
 	hw->parent = &spi->dev;
 
+	/* Read device ID register */
+	rc = dw1000_read(dw, DW1000_DEV_ID, 0, &id, sizeof(id));
+	if (rc) {
+		dev_err(&spi->dev, "could not read device ID: %d\n", rc);
+		goto err_dev_id_read;
+	}
+	dev_info(&spi->dev, "found %04X model %02X version %02X\n",
+		 le16_to_cpu(id.ridtag), id.model, id.ver_rev);
+	if (id.ridtag != cpu_to_le16(DW1000_RIDTAG_MAGIC)) {
+		dev_err(&spi->dev, "incorrect RID tag %04X (expected %04X)\n",
+			le16_to_cpu(id.ridtag), DW1000_RIDTAG_MAGIC);
+		rc = -EINVAL;
+		goto err_dev_id_mismatch;
+	}
+
 	/* Initialise register map */
 	rc = dw1000_regmap_init(dw);
 	if (rc)
 		goto err_regmap_init;
-
-	/* Read device ID register */
-	rc = regmap_read(dw->regs.dword, DW1000_DEV_ID, &id);
-	if (rc) {
-		dev_err(&spi->dev, "could not read device ID: %d\n", rc);
-		goto err_dev_id;
-	}
-	dev_info(&spi->dev, "found %08X using IRQ %d\n", id, spi->irq);
 
 	/* Register IEEE 802.15.4 device */
 	rc = ieee802154_register_hw(hw);
@@ -152,7 +401,8 @@ static int dw1000_probe(struct spi_device *spi)
 
 	ieee802154_unregister_hw(hw);
  err_register_hw:
- err_dev_id:
+ err_dev_id_mismatch:
+ err_dev_id_read:
  err_regmap_init:
 	ieee802154_free_hw(hw);
  err_alloc_hw:
--- kernel/dw1000.h
+++ kernel/dw1000.h
@@ -26,25 +26,184 @@
 #include <linux/device.h>
 #include <linux/regmap.h>
 
+/* SPI bus header */
+struct dw1000_spi_header {
+	/* Register file and command */
+	uint8_t command;
+	/* Low order 7 bits and extended address indicator */
+	uint8_t low;
+	/* High order 8 bits */
+	uint8_t high;
+} __packed;
+
+/* SPI bus header command flags */
+#define DW1000_WRITE 0x80
+#define DW1000_OFFSET 0x40
+
+/* SPI bus header extended offset present flags */
+#define DW1000_EXTENDED 0x80
+
+/* SPI bus header extended offset shift */
+#define DW1000_EXTENDED_SHIFT 7
+
+/* Register files */
+#define DW1000_DEV_ID		0x00
+#define DW1000_DEV_ID_LEN	4
+#define DW1000_EUI		0x01
+#define DW1000_EUI_LEN		8
+#define DW1000_PANADR		0x03
+#define DW1000_PANADR_LEN	4
+#define DW1000_SYS_CFG		0x04
+#define DW1000_SYS_CFG_LEN	4
+#define DW1000_SYS_TIME		0x06
+#define DW1000_SYS_TIME_LEN	5
+#define DW1000_TX_FCTRL		0x08
+#define DW1000_TX_FCTRL_LEN	5
+#define DW1000_TX_BUFFER	0x09
+#define DW1000_TX_BUFFER_LEN	1024
+#define DW1000_DX_TIME		0x0a
+#define DW1000_DX_TIME_LEN	5
+#define DW1000_RX_FWTO		0x0c
+#define DW1000_RX_FWTO_LEN	2
+#define DW1000_SYS_CTRL		0x0d
+#define DW1000_SYS_CTRL_LEN	4
+#define DW1000_SYS_MASK		0x0e
+#define DW1000_SYS_MASK_LEN	4
+#define DW1000_SYS_STATUS	0x0f
+#define DW1000_SYS_STATUS_LEN	5
+#define DW1000_RX_FINFO		0x10
+#define DW1000_RX_FINFO_LEN	4
+#define DW1000_RX_BUFFER	0x11
+#define DW1000_RX_BUFFER_LEN	1024
+#define DW1000_RX_FQUAL		0x12
+#define DW1000_RX_FQUAL_LEN	8
+#define DW1000_RX_TTCKI		0x13
+#define DW1000_RX_TTCKI_LEN	4
+#define DW1000_RX_TTCKO		0x14
+#define DW1000_RX_TTCKO_LEN	5
+#define DW1000_RX_TIME		0x15
+#define DW1000_RX_TIME_LEN	14
+#define DW1000_TX_TIME		0x17
+#define DW1000_TX_TIME_LEN	10
+#define DW1000_TX_ANTD		0x18
+#define DW1000_TX_ANTD_LEN	2
+#define DW1000_ACK_RESP_T	0x1a
+#define DW1000_ACK_RESP_T_LEN	4
+#define DW1000_RX_SNIFF		0x1d
+#define DW1000_RX_SNIFF_LEN	4
+#define DW1000_TX_POWER		0x1e
+#define DW1000_TX_POWER_LEN	4
+#define DW1000_CHAN_CTRL	0x1f
+#define DW1000_CHAN_CTRL_LEN	4
+#define DW1000_USR_SFD		0x21
+#define DW1000_USR_SFD_LEN	41
+#define DW1000_AGC_CTRL		0x23
+#define DW1000_AGC_CTRL_LEN	33
+#define DW1000_EXT_SYNC		0x24
+#define DW1000_EXT_SYNC_LEN	12
+#define DW1000_ACC_MEM		0x25
+#define DW1000_ACC_MEM_LEN	4064 /* sic */
+#define DW1000_GPIO_CTRL	0x26
+#define DW1000_GPIO_CTRL_LEN	44
+#define DW1000_DRX_CONF		0x27
+#define DW1000_DRX_CONF_LEN	44
+#define DW1000_RF_CONF		0x28
+#define DW1000_RF_CONF_LEN	58
+#define DW1000_TX_CAL		0x2a
+#define DW1000_TX_CAL_LEN	52
+#define DW1000_FS_CTRL		0x2b
+#define DW1000_FS_CTRL_LEN	21
+#define DW1000_AON		0x2c
+#define DW1000_AON_LEN		12
+#define DW1000_OTP_IF		0x2d
+#define DW1000_OTP_IF_LEN	18
+#define DW1000_LDE_CTRL		0x2e
+#define DW1000_LDE_CTRL_LEN	0x2806
+#define DW1000_DIG_DIAG		0x2f
+#define DW1000_DIG_DIAG_LEN	41
+#define DW1000_PMSC		0x36
+#define DW1000_PMSC_LEN		48
+
+/* Device ID register file layout */
+struct dw1000_dev_id {
+	/* Version and revision */
+	uint8_t ver_rev;
+	/* Model */
+	uint8_t model;
+	/* Register identification tag */
+	uint16_t ridtag;
+} __packed;
+
+/* Expedit register identification tag */
+#define DW1000_RIDTAG_MAGIC 0xdeca
+
 /* Register map parameters */
-#define DW1000_WRITE_FLAG_MASK 0x80
-#define DW1000_READ_FLAG_MASK 0x00
-#define DW1000_DWORD_REG_BITS 6
-#define DW1000_DWORD_PAD_BITS 2
-#define DW1000_DWORD_VAL_BITS 32
+#define DW1000_REG_BITS 16
+#define DW1000_VAL_BITS 8
 
-/* Device ID register */
-#define DW1000_DEV_ID 0x00
+/* Register map descriptor */
+struct dw1000_regmap_config {
+	/* Offset within DW1000 device structure */
+	size_t offset;
+	/* Register file */
+	unsigned int file;
+	/* Register offset shift */
+	unsigned int shift;
+	/* Register map configuration */
+	struct regmap_config config;
+};
 
-/* Register maps */
-struct dw1000_registers {
-	struct regmap *dword;
+/* Register map */
+struct dw1000_regmap {
+	/* Register map */
+	struct regmap *regs;
+	/* Containing DW1000 device */
+	struct dw1000 *dw;
+	/* Register map configuration */
+	const struct dw1000_regmap_config *config;
 };
 
 /* DW1000 device */
 struct dw1000 {
 	struct spi_device *spi;
-	struct dw1000_registers regs;
+	struct dw1000_regmap dev_id;
+	struct dw1000_regmap eui;
+	struct dw1000_regmap panadr;
+	struct dw1000_regmap sys_cfg;
+	struct dw1000_regmap sys_time;
+	struct dw1000_regmap tx_fctrl;
+	struct dw1000_regmap tx_buffer;
+	struct dw1000_regmap dx_time;
+	struct dw1000_regmap rx_fwto;
+	struct dw1000_regmap sys_ctrl;
+	struct dw1000_regmap sys_mask;
+	struct dw1000_regmap sys_status;
+	struct dw1000_regmap rx_finfo;
+	struct dw1000_regmap rx_buffer;
+	struct dw1000_regmap rx_fqual;
+	struct dw1000_regmap rx_ttcki;
+	struct dw1000_regmap rx_ttcko;
+	struct dw1000_regmap rx_time;
+	struct dw1000_regmap tx_time;
+	struct dw1000_regmap tx_antd;
+	struct dw1000_regmap ack_resp_t;
+	struct dw1000_regmap rx_sniff;
+	struct dw1000_regmap tx_power;
+	struct dw1000_regmap chan_ctrl;
+	struct dw1000_regmap usr_sfd;
+	struct dw1000_regmap agc_ctrl;
+	struct dw1000_regmap ext_sync;
+	struct dw1000_regmap acc_mem;
+	struct dw1000_regmap gpio_ctrl;
+	struct dw1000_regmap drx_conf;
+	struct dw1000_regmap rf_conf;
+	struct dw1000_regmap tx_cal;
+	struct dw1000_regmap fs_ctrl;
+	struct dw1000_regmap aon;
+	struct dw1000_regmap otp_if;
+	struct dw1000_regmap lde_ctrl;
+	struct dw1000_regmap dig_diag;
+	struct dw1000_regmap pmsc;
 };
 
 #endif /* __DW1000_H */
