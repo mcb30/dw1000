fa4e4a2dbf09c6db8b657aefb3d1d190d9661266
Petri Mattila <petri.mattila@unipart.io>
2018-08-13 12:23:48 +0100

    eeprom: Add calibration and programming automation
    Add ANTD16, ANTD64, XTALT, and SPIMAX parameters in the Makefile.
    Add 'program' target in the Makefile, and piprog.sh script for
    flashing the RPi 3B Tail hats.
    
    Signed-off-by: Petri Mattila <petri.mattila@unipart.io>

---
+++ rpi/eeprom/piprog.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+FILE="$1"
+SIZE=$(stat -c'%s' "$1")
+
+CHIP=${CHIP:-24c64}
+
+I2C_BUS=${I2C_BUS:-0}
+I2C_ADDR=${I2C_ADDR:-50}
+
+SYS=/sys/class/i2c-adapter/i2c-${I2C_BUS}
+DEV=${SYS}/${I2C_BUS}-00${I2C_ADDR}
+
+modprobe i2c_dev  || exit $?
+modprobe at24     || exit $?
+
+test -f ${FILE}   || exit 1
+test -d ${SYS}    || exit 1
+
+if [ ! -d ${DEV} ]
+then
+    echo "${CHIP} 0x${I2C_ADDR}" > ${SYS}/new_device
+fi
+
+dd if="${FILE}" of=${DEV}/eeprom oflag=sync status=none
+
+cmp -n ${SIZE} -s ${FILE} ${DEV}/eeprom ; RET=$?
+
+echo "0x${I2C_ADDR}" > ${SYS}/delete_device
+
+rmmod at24
+rmmod i2c_dev
+
+exit ${RET}
